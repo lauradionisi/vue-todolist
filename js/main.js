4fdeeb6d50f7de06ea4095ba36e24f0a71971482
Laura <laura.dionisi91@gmail.com>
2021-03-22 15:13:25 +0100

    vue todolist

---
+++ js/main.js
@@ -0,0 +1,32 @@
+Vue.config.devtools = true;
+
+var app = new Vue ({
+    el: '#app',
+    data : {
+        newTask: '',
+        tasks: [
+            'Svegliarsi',
+            'Fare Colazione', 
+            'Imparare Javascript'
+        ]        
+    },
+
+    methods: {
+
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = '';
+        },
+    
+        removeTask(index) {
+            this.tasks.splice(index, 1)
+        },
+
+        
+    }
+    
+        
+
+})
+console.log(app);
+
